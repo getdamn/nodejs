ec1cb46bf2edbda30a0ac2051e038477a853b302
getdamn <n1mb0606@gmail.com>
2018-06-14 17:33:08 +0900

    Name: test.js Date: 2018/06/14

--- test.js
+++ test.js
@@ -1,6 +1,16 @@
 var http = require('http');
 http.createServer(function (req,res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write("<h1>Hello World</h1>");
+	var str="";
+	str += "<html><body><table border='1';>";
+	for(i in req.headers){
+		str += "<tr><td>";
+		str += i;
+		str += "</td><td>";
+		str += req.headers[i];
+		str += "</td></tr>";
+		}
+	str += "</table></body></html>";
+	res.write(str);
 	res.end();
 }).listen(8080);
